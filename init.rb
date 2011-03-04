7061f6ab2ddb9b8ed81d5849d69c917d08adfe18
Alex Shulgin <ash@commandprompt.com>
2011-03-04 21:22:10 +0200

    Renamed the project-level tab to 'Pastebin'.  Now landing directly to new paste form.

--- init.rb
+++ init.rb
@@ -49,8 +49,8 @@ Redmine::Plugin.register :redmine_pastebin do
     permission :delete_pastes, :pastes => [:destroy]
   end
 
-  menu :project_menu, :pastes, { :controller => 'pastes', :action => 'index' },
-    :caption => 'Pastes', :after => 'Wiki', :param => :project_id
+  menu :project_menu, :pastes, { :controller => 'pastes', :action => 'new' },
+    :caption => 'Pastebin', :after => 'Wiki', :param => :project_id
 end
 
 Redmine::Activity.map do |activity|
