eadf596b59d37c3c4b71d67f3552d9c295383bb5
Alex Shulgin <ash@commandprompt.com>
2011-09-26 11:26:07 +0300

    Fixed hook partial: unique name.

--- lib/redmine_pastebin/view_hooks.rb
+++ lib/redmine_pastebin/view_hooks.rb
@@ -1,6 +1,6 @@
 module RedminePastebin
   class ViewHooks < Redmine::Hook::ViewListener
     render_on :view_projects_index_activity_menu,
-      :partial => 'hooks/view_projects_index_activity_menu'
+      :partial => 'hooks/redmine_pastebin_all_pastes_link'
   end
 end
